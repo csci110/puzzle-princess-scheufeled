8778b27ca5128611968415294823097e7abf28f9
Dawson <scheufeled@dewv.edu>
2018-11-14 17:28:33 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -11,8 +11,21 @@ class TicTacToe extends game {
         this.squareSize = 150; // the number of pixels on each side of a board square
         this.size = 3; //set size of board 3 by 3
         this.activeMarker; // variable exists, but value is undefined
-    } 
+        this.dataModel = [];
+        for (let row = 0; row < this.size; row = row + 1) {
+            this.dataModel[row] = [];
+            for (let col = 0; col < this.boardSize; col = col + 1) {
+                this.dataModel[row][col] = this.emptySquareSymbol;
+            } 
+        }
+        this.emptySquareSymbol = "-";
+    }
+     takeTurns() {
+        this.activeMarker = new PrincessMarker(this);
+    }
 }
+let theBoard = new TicTacToe();
+theBoard.takeTurns();
 
 class Marker extends game {
     constructor( board, image, name) {
@@ -20,10 +33,43 @@ class Marker extends game {
         this.board = board;
         this.name = name;
         this.image = image;
+        this.x = this.startX = 150;
+        this.y = this.startY = 150;
         this.x = 150;
         this.y = 275;
     }
+    playInSquare(row, col) {
+        this.x = this.board.x + col * this.board.squareSize + this.board.squareSize / 2 - this.width / 2;
+        this.y = this.board.y + row * this.board.squareSize + this.board.squareSize / 2 - this.height / 2;
+    }
+}
+class PrincessMarker extends Marker {
+    constructor(board) {
+        super(board, "annFace.png", "Princess Ann");
+        this.dragging = false;
+        this.board.x;
+    }
+    handleMouseLeftButtonDown() {
+        this.dragging = true;
+    }
+    handleMouseLeftButtonUp() {
+        this.dragging = false;
+        let row = Math.floor((game.getMouseY() - this.board.y) / this.board.squareSize);
+        let col = Math.floor((game.getMouseX() - this.board.x) / this.board.squareSize);
+        console.log(row, ",", col);
+        if (row < 0 || row >= this.board.size || col < 0 || col >= this.board.size) {
+            this.x = this.startX;
+            this.y = this.startY;
+            return;
+        }
+        this.playInSquare(row, col);
+        this.board.takeTurns();
+        }
+    handleGameLoop() {
+        if (this.dragging === true) {
+            this.x = game.getMouseX() - this.width / 2;
+            this.y = game.getMouseY() - this.height / 2;
+        }
+    }
 }
 
-let theBoard = new TicTacToe();
-theBoard.takeTurns();
